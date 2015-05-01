b840a5d9ab60f2da86adfcffe65b18ad7c230aa6
unknown <x54856@USMANBUX54856.usma.ds.army.edu>
2015-05-01 07:51:27 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Groupwork
+# Readme
+Mess Hall Signup App is located at:
+limitless-craig-2971.kerokuapp.com
--- mealsignups/settings.py
+++ mealsignups/settings.py
@@ -10,8 +10,13 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import os.path
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    '/signup/static',
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -37,6 +42,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'signup',
+	'bootstrap',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -79,4 +85,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+
